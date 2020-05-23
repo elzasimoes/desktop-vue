1c0e395c693006a2b803b208ab98e61bd4e8e7d0
Elza Simões <elzaesimoes@gmail.com>
2020-05-23 17:31:40 -0400

    Graphical Interface #03

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+<v-app-bar app color="#dc143c" dark>
       
-<v-toolbar-title>Most Used Words</v-toolbar-title>
+<v-toolbar-title><strong>Most Used Words</strong></v-toolbar-title>
 
 
 
@@ -31,3 +31,4 @@ export default {
   }),
 };
 </script>
+
--- src/backend/index.js
+++ src/backend/index.js
@@ -2,23 +2,14 @@ const { ipcMain } = require('electron')
 
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
 
     pathsToRows(paths)
         .then(rows => prepareData(rows))
         .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitles', [
-                { name: 'you', amount: 900 },
-                { name: 'her', amount: 853 },
-                { name: 'i', amount: 1222 },
-                { name: 'he', amount: 853 },
-                { name: 'our', amount: 1222 },
-                { name: 'he', amount: 853 },
-                { name: 'after', amount: 1222 },
-                { name: 'moon', amount: 853 },
-                { name: 'house', amount: 1222 }
-            ])
-        })
+        .then(groupedWords => event.reply('process-subtitles', groupedWords))
+
+
 })
