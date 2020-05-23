6027cee2628a62b4d8582549cea36bae852239cf
Elza Simões <elzaesimoes@gmail.com>
2020-05-23 13:34:18 -0400

    Processing Files # 01

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,17 +1,22 @@
 const { ipcMain } = require('electron')
 
-ipcMain.on('process-subtitles', (event, paths) => {
-    console.log(paths)
+const pathsToRows = require('./pathsToRows')
 
-    event.reply('process-subtitles', [
-        { name: 'you', amount: 900 },
-        { name: 'her', amount: 853 },
-        { name: 'i', amount: 1222 },
-        { name: 'he', amount: 853 },
-        { name: 'our', amount: 1222 },
-        { name: 'he', amount: 853 },
-        { name: 'after', amount: 1222 },
-        { name: 'moon', amount: 853 },
-        { name: 'house', amount: 1222 }
-    ])
+ipcMain.on('process-subtitles', (event, paths) => {
+
+    pathsToRows(paths)
+        .then(rows => console.log(rows))
+        .then(() => {
+            event.reply('process-subtitles', [
+                { name: 'you', amount: 900 },
+                { name: 'her', amount: 853 },
+                { name: 'i', amount: 1222 },
+                { name: 'he', amount: 853 },
+                { name: 'our', amount: 1222 },
+                { name: 'he', amount: 853 },
+                { name: 'after', amount: 1222 },
+                { name: 'moon', amount: 853 },
+                { name: 'house', amount: 1222 }
+            ])
+        })
 })
